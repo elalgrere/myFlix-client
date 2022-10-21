3dcb3665602a509e2b7f4d3b3d7ebf656623b53f
elalgrere <108877252+elalgrere@users.noreply.github.com>
2022-10-21 17:49:59 +0200

    Updated localhost

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import axios from 'axios';
-
-import { MovieCard } from '../movie-card/movie-card';
-import { MovieView } from '../movie-view/movie-view';
+//import axios from 'axios';
+import ReactDOM from 'react-dom';
+import MainView from './components/main-view/main-view';
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
-
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
   
@@ -17,19 +15,20 @@ class MyFlixApplication extends React.Component {
       selectedMovie: null
     }
   }
-  
+ 
     render() {
-      return <div>Hello World</div>;
-    }
-  
-    componentDidMount() {
-      // code executed right after the component is added to the DOM
+      return (
+        <MainView />
+      );
     }
   }
+    
+ 
   
-
 // Finds the root of your app
 const container = document.getElementsByClassName('app-container')[0];
 
 // Tells React to render your app in the root DOM element
 ReactDOM.render(React.createElement(MyFlixApplication), container);
+
+export default MainView;
