83ae7edd172d7192469b2498a8b7ee77c89d6a44
elalgrere <108877252+elalgrere@users.noreply.github.com>
2022-10-27 13:32:49 +0200

    Updated exercise 3.5

---
+++ src/components/registration-view/registration-view.jsx
@@ -0,0 +1,101 @@
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
+import { Form, Button, Card, CardGroup, Container, Col, Row} from 'react-bootstrap'; 
+import './registration-view.scss';
+import axios from 'axios';
+
+export function RegistrationView(props) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [birthday, setBirthday] = useState("");
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post('https://mymoviesapp.herokuapp.com/users', {
+      Username: username,
+      Password: password,
+      Email: email,
+      Birthday: birthday
+    })
+    .then(response => {
+      const data = response.data;
+      console.log(data);
+      window.open('/', '_self');
+  })
+    .catch(e => {
+      console.log('error registering the user');
+      alert('Something wasn\'t entered right');
+    });
+  };
+
+  return (
+    <Container>
+      <Row>
+       <Col>
+        <CardGroup>
+         <Card> 
+         <Card.Body> 
+         <Card.Title>Please Register</Card.Title> 
+        <Form>
+         <Form.Group>
+         <Form.Label>Username:</Form.Label>
+         <Form.Control
+         type="text" 
+         value={username}
+         onChange={e => setUsername(e.target.value)}
+         required
+         placeholder="Enter username"
+         />
+      </Form.Group>  
+
+      <Form.Group> 
+       <Form.Label>Password:</Form.Label>
+        <Form.Control 
+        type="password" 
+        value={password} 
+        onChange={e => setPassword(e.target.value)} 
+        required
+        minLenght="8"
+        placeholder="Your password must be 8 or more characters"
+        />
+        </Form.Group> 
+
+       <Form.Group>  
+      <Form.Label>Email:</Form.Label>
+        <Form.Control
+        type="email" 
+        value={email} 
+        onChange={e => setEmail(e.target.value)} 
+        required
+        placeholder="Enter your email address"
+        />
+        </Form.Group>
+        <Form.Group>
+      <Form.Label>Birthday:</Form.Label>
+        <Form.Control
+        type="birthday" 
+        value={birthday} 
+        onChange={e => setBirthday(e.target.value)} 
+        />
+      </Form.Group>
+      <Button 
+      variant= "primary"
+      type="submit" 
+      onClick={handleSubmit}>
+       Submit
+          </Button>  
+         </Form> 
+         </Card.Body>  
+         </Card>
+        </CardGroup>
+       </Col>
+      </Row> 
+    </Container>
+  ); 
+}
+
+
+RegistrationView.propTypes = {
+  onRegistration: PropTypes.func.isRequired,
+};
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+<<<<<<< Updated upstream
 import ReactDOM from 'react-dom/client';
+=======
+import ReactDOM from 'react-dom';
+>>>>>>> Stashed changes
 import {MainView} from './components/main-view/main-view';
+import Container from 'react-bootstrap/Container';
 
-// Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
 
 // Main component (will eventually use all the others)
+
 class MyFlixApplication extends React.Component {
+<<<<<<< Updated upstream
   render() {
     return (
       <MainView />
@@ -14,10 +20,27 @@ class MyFlixApplication extends React.Component {
   }
 }
 
+=======
+  render() {  
+    return (
+    <Container>
+      <MainView />
+    </Container>
+  );
+ }
+} 
+    
+ 
+  
+>>>>>>> Stashed changes
 // Finds the root of your app
 const container = document.getElementById('app-container')
 console.log ('cot: ', container)
 const rootElement =ReactDOM.createRoot(container)
 
 // Tells React to render your app in the root DOM element
+<<<<<<< Updated upstream
 rootElement.render(<MyFlixApplication/>)
+=======
+ReactDOM.render(React.createElement(MyFlixApplication), container);
+>>>>>>> Stashed changes
