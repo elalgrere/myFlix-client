c741cbcd02a2d4b077abc41d412a247a5dd87b81
elalgrere <108877252+elalgrere@users.noreply.github.com>
2022-10-27 13:54:57 +0200

    Updated exercise 3.5

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-<<<<<<< Updated upstream
 import ReactDOM from 'react-dom/client';
-=======
 import ReactDOM from 'react-dom';
->>>>>>> Stashed changes
+
 import {MainView} from './components/main-view/main-view';
 import Container from 'react-bootstrap/Container';
 
@@ -12,15 +10,7 @@ import './index.scss';
 // Main component (will eventually use all the others)
 
 class MyFlixApplication extends React.Component {
-<<<<<<< Updated upstream
-  render() {
-    return (
-      <MainView />
-    );
-  }
-}
-
-=======
+ 
   render() {  
     return (
     <Container>
@@ -28,19 +18,14 @@ class MyFlixApplication extends React.Component {
     </Container>
   );
  }
-} 
-    
+}
  
-  
->>>>>>> Stashed changes
+    
 // Finds the root of your app
 const container = document.getElementById('app-container')
 console.log ('cot: ', container)
 const rootElement =ReactDOM.createRoot(container)
 
 // Tells React to render your app in the root DOM element
-<<<<<<< Updated upstream
 rootElement.render(<MyFlixApplication/>)
-=======
 ReactDOM.render(React.createElement(MyFlixApplication), container);
->>>>>>> Stashed changes
